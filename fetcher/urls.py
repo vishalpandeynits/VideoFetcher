4d2e259db2942378ee601bd7995cea019be20996
Vishal <vishalpandeyviptsk@gmail.com>
2021-12-18 20:18:37 +0530

    improved UI

--- fetcher/urls.py
+++ fetcher/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.VideoList.as_view(), name = "video_list"),
     path('api/', views.VideoListAPI.as_view(), name = "video_list_api"),
-    path('dashboard/', views.VideoListGenericAPI.as_view())
+    path('dashboard/', views.VideoListGenericAPI.as_view(), name = "dashboard")
 ]
